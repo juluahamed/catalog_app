f3f93014240f01e9c1a9579a8343978b5585af36
Julu Ahamed <juluahamed@gmail.com>
2017-10-10 17:18:12 +0530

    Fixing Template issue 2

--- wasp/views.py
+++ wasp/views.py
@@ -7,5 +7,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return render(request, 'wasp/base.html')
+	return render(request, 'base.html')
     #return HttpResponse("Rango says hey there partner!")
