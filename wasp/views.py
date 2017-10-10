beca94d648aa5b0b0d25116ea92d85df81333d32
Julu Ahamed <juluahamed@gmail.com>
2017-10-10 17:04:54 +0530

    Add base.html

--- wasp/views.py
+++ wasp/views.py
@@ -7,4 +7,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+	return render(request, 'wasp/base.html')
+    #return HttpResponse("Rango says hey there partner!")
